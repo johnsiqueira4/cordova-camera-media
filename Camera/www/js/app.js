21e6b1028529784e46743a12be743798aed5ad39
Jonatha Breno <jbreno@technisys.com>
2022-08-11 01:59:36 -0300

    ajustes video

--- Camera/www/js/app.js
+++ Camera/www/js/app.js
@@ -7,7 +7,7 @@ function onDeviceReady() {
 }
 
 function cameraApp() {
-    navigator.camera.getPicture(onSuccess, onFail, {
+    navigator.camera.getPicture(onPictureSuccess, onPictureFail, {
         quality: 100,
         destinationType: Camera.DestinationType.FILE_URI,
         saveToPhotoAlbum: true,
@@ -21,17 +21,17 @@ function cameraApp() {
     })
 }
 
-function onSuccess(imageURI) {
+function onPictureSuccess(imageURI) {
 
     alert("imageData " + imageURI);
     // document.getElementById('imagem').src = imageURI;
     // document.querySelector('url').innerHT = imageURI;
 
-    window.resolveLocalFileSystemURL(imageURI, resolveOnSuccess, resOnError)
+    window.resolveLocalFileSystemURL(imageURI, resolvePictureOnSuccess, resOnError)
 
 }
 
-function onFail(message) {
+function onPictureFail(message) {
     alert('Failed because: ' + message);
 }
 
@@ -39,7 +39,7 @@ function resOnError(error) {
     alert('resOnError: ' + error.code);
 }
 
-function resolveOnSuccess(entry) {
+function resolvePictureOnSuccess(entry) {
     entry.file(function (fileInput) {
         let reader = new FileReader();
         reader.onloadend = function (evt) {
@@ -56,7 +56,7 @@ let captureSuccess = function(mediaFiles) {
     let i, path, len;
     for (i = 0, len = mediaFiles.length; i < len; i++) {
         path = mediaFiles[i].fullPath;
-        alert("path img:> " + path);
+        window.resolveLocalFileSystemURL(path, resolveVideoOnSuccess, resOnError)
     }
 }
 
@@ -64,8 +64,19 @@ let captureError = function(error) {
     navigator.notification.alert('Error code: ' + error.code, null, 'Capture Error');
 };
 
+function resolveVideoOnSuccess(entry) {
+    entry.file(function (fileInput) {
+        let reader = new FileReader();
+        reader.onloadend = function (evt) {
+            let base64 = evt.target.result;
+            alert("base64 " + base64);
+        };
+        reader.readAsDataURL(fileInput);
+    }, function () { alert('fail on trying to read the file.') });
+}
+
 function mediaCaptureApp() {
     navigator.device.capture.captureVideo(
-        captureSuccess, captureError
+        captureSuccess, captureError, { duration: 20 }
     );
 }
